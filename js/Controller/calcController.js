50c421bc7a0d1bed60f6f7a7b064a0a6592dced7
Adelto Oliveira Almeida <adtguitar@gmail.com>
2019-04-28 23:15:02 -0300

    criando diretorio Controller e add a class calcControler, criando os gets e sets e seus atributos e o metodo initialize

--- js/Controller/calcController.js
+++ js/Controller/calcController.js
@@ -1,4 +1,34 @@
 class CalcController {
-    //Atributos
-    //Metodos
+    //ATRIBUTOS
+    constructor(){
+        this._displayCalc = 0;
+        this._currentDate;
+        this.initialize();
+    }
+
+    //GETTERS E SETTERS
+    get displayCalc(){
+        return this._displayCalc;
+    }
+    set displayCalc(value){
+        this._displayCalc = value;
+    }
+    
+    get currentDate(){
+        return this._currentDate;
+    }
+    set currentDate(value){
+        this._currentDate = value;
+    }
+    
+    //METODOS
+    initialize(){
+        let $displayCalc = document.querySelector('#display');
+        let $date =  document.querySelector('#data');
+        let $time = document.querySelector('#time');
+
+        $displayCalc.innerHTML = "1010";
+        $date.innerHTML = "10/10/2019";
+        $time.innerHTML = "22:59";
+    }
 }
