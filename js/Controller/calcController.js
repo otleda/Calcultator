9e8edf84e9631a4c6bcbe9ae4704a480a613ec22
Adelto Oliveira Almeida <adtguitar@gmail.com>
2019-05-07 09:41:34 -0300

    Refatorando o codigo, e criando o metodo de criar mais de um evento para um função com addEventListenerAll na classe

--- js/Controller/calcController.js
+++ js/Controller/calcController.js
@@ -1,20 +1,45 @@
 class CalcController {
     //ATRIBUTOS
     constructor(){
-        this._locale = "pt-BR";
+        this._locale = 'pt-BR';
         this._$display = document.querySelector('#display');
         this._$date =  document.querySelector('#data');
         this._$time = document.querySelector('#time');
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
     
     //METODOS
     initialize(){
+
+        this.setDisplayDateTime();
+        
         setInterval(() => {
-            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
-            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+            this.setDisplayDateTime();
         }, 1000);
+    }
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+    
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach( event => {
+            element.addEventListener(event, fn, false);
+        });
+
+    }
+
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll('.col');
+        buttons.forEach( btn => {
+            this.addEventListenerAll(btn, 'click drag', event => {
+                console.log(btn.className.replace("col","").replace("btn-",""));
+            });
+        });
     }
 
     //GETTERS E SETTERS
