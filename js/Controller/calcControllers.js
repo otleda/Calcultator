dd8648e5a079bb28b23f9bc9b36eed7dabc0b171
Adelto Oliveira Almeida <adtguitar@gmail.com>
2021-03-25 01:13:45 -0300

    Refatorando o codigo, add funcao initialize e setInterval nativa do js, para apresentar a data e a hora local no dipslay

--- js/Controller/calcControllers.js
+++ js/Controller/calcControllers.js
@@ -1,32 +1,29 @@
 class CalcControllers {
     constructor() {
-        this._displayCalc = "0";
+        this._locale;
+        this._displayCalcEl = document.querySelector('#display');
+        this._dateEl = document.querySelector('#data');
+        this._timeEl = document.querySelector('#time');
         this._currentDate;
         this.initialize();
     }
 
-    get displayCalc() { 
-    return this._displayCalc;
-    }
-    set displayCalc(value) {
-    this._displayCalc = value;
-    }
-
-    get currentDate() {
-    return this._currentDate
-    }
-    set currentDate(value) {
-    this._currentDate = value;
-    }
-
     initialize() {
-        let displayCalcEl = document.querySelector('#display');
-        let dataEl = document.querySelector('#data');
-        let timeEl = document.querySelector('#time');
-
-        displayCalcEl.innerHTML = '1234567890';
-        dataEl.innerHTML = '02/07/1985';
-        timeEl.innerHTML = '00:00';
+        setInterval(()=>{
+          this.dipslayDate = this.currentDate.toLocaleDateString(this._locale);
+          this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+        }, 1000);
     }
-     
+
+    get displayCalc() { return this._displayCalcEl.innerHTML; }
+    set displayCalc(value) { this._displayCalcEl.innerHTML = value; }
+
+    get currentDate() { return new Date(); }
+    set currentDate(value) { this._currentDate = value; }
+ 
+    get dipslayDate() { return this._dateEl.innerHTML; }
+    set dipslayDate(value) { this._dateEl.innerHTML = value; }
+
+    get displayTime() { return this._timeEl.innerHTML; }
+    set displayTime(value) { this._timeEl.innerHTML = value; }
 }
