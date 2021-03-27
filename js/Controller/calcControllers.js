7105e19d2207fb5bef555f4b97b653936f2d7d59
Adelto Oliveira Almeida <adtguitar@gmail.com>
2021-03-27 19:56:53 -0300

    Refatorando o codigo, criando acoes nos buttons, criando + uma funcao para a classe CalcController

--- js/Controller/calcControllers.js
+++ js/Controller/calcControllers.js
@@ -1,19 +1,42 @@
 class CalcControllers {
     constructor() {
-        this._locale;
+        this._locale = 'pt-br';
         this._displayCalcEl = document.querySelector('#display');
         this._dateEl = document.querySelector('#data');
         this._timeEl = document.querySelector('#time');
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
+    }
+
+    setIntervalDisplay() {
+        this.dipslayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
     initialize() {
+        this.setIntervalDisplay();
+        
         setInterval(()=>{
-          this.dipslayDate = this.currentDate.toLocaleDateString(this._locale);
-          this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+            this.setIntervalDisplay();
         }, 1000);
     }
+
+    addEventListenerAll(element, events, fn) {
+        events.split(' ').forEach((event)=>{
+            element.addEventListener(event, fn);
+        });
+    }
+
+    initButtonsEvents() {
+        let buttons =  document.querySelectorAll('#buttons .col');
+
+        buttons.forEach(button => {
+            this.addEventListenerAll(button, 'click drag', () =>{
+                console.log(button.className.replace('col btn-',''));
+            });
+        });
+    }   
 
     get displayCalc() { return this._displayCalcEl.innerHTML; }
     set displayCalc(value) { this._displayCalcEl.innerHTML = value; }
