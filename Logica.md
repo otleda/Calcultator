c7d4ef005ce554d5e7308e85b9e77c8eafd9dc04
Adelto Oliveira Almeida <adtguitar@gmail.com>
2021-04-04 23:36:10 -0300

    criado uma Array, para armazenar as entradas dos dados digitado na calculadora, Pegando os nomes das classes da Calculadora,  add Evento na acao do click, formatando os dados, criando o metodos ClearEntry(), Clear(), execBtn(), addOperation(), Fazer uma verificacao quando o usuario digitar numeros ou operadores, se for numero, concatenar os numeros [Obs: usar o metodo nativo toString para converte, para concatenar], e add a array *operation[]* se for operadores deixa na memoria o numero e espera o proximo digito, Criando um Metodo para pegar o ultimo dado da array, criando um metodo que verfica se estamos digitando numeros, ou sinais com a funcao nativa do js isNAN()

---
+++ Logica.md
@@ -0,0 +1,33 @@
+
+- [ OK ] Criar uma Classe para controlar a calculadora.
+
+- [ OK ] Criando Variaves Globais, e pegando classes com o metodo QueySelector().
+
+- [ OK ] Trabalhando com Data e Hora da Calculadora.
+
+- [ OK ] add o metodo construtor, e add Variaveis(Atributos) e Funcoes(Metodos).
+
+- [ OK ] Criar os Geteres e Seteres, os metodos acessores.
+
+- [ OK ] Criar o metodo de inicializacao, e tratando a data e hora para a regiao local com a classe nativa Date().
+
+- [ OK ] Criar uma Array, para armazenar as entradas dos dados digitado na calculadora.
+
+- [ OK ] Pegando os nomes das classes da Calculadora,  add Evento na acao do click, formatando os dados.
+
+- [ OK ] Criando Metodo para dar acoes aos Botoes no click, dando as funcoes para cada botao
+
+- [ OK ] Criando metodo Clear(), para limpar todos os dados.
+
+- [ OK ] Criando metodo CLearEntry(), para limpar os dados da tela.
+
+- [ OK ] Criando o Metodo execBtn() e addOperation() e populando a array operation. 
+
+- [ OK ] Converte os sinais (soma, subtracao ...), por simbolos
+
+- [ OK ] Fazer uma verificacao quando o usuario digitar numeros ou operadores, se for numero, concatenar os numeros [Obs: usar o metodo nativo toString para converte, para concatenar], e add a array *operation[]* se for operadores deixa na memoria o numero e espera o proximo digito.
+
+- [ OK ] Criar um metodo para pegar o ultimo dado digitado dentro da Array.
+
+- [  ] Criar um Metodo que verifica se estamos digitamos numero, ou sinais na calculadora, com a funcao nativa do javaScript isNAN() => ele diz se quando nao e um numero.
+- [  ] Criar uma forma de quando popular na array da calculadora, quando for ate 4 indice somar na array exemplo [(300,"+",140), "-"], mostrar o resultado na tela 440
--- js/Controller/calcControllers.js
+++ js/Controller/calcControllers.js
@@ -1,5 +1,6 @@
 class CalcControllers {
     constructor() {
+        this._operation = [];
         this._locale = 'pt-br';
         this._displayCalcEl = document.querySelector('#display');
         this._dateEl = document.querySelector('#data');
@@ -28,25 +29,126 @@ class CalcControllers {
         });
     }
 
+    clearAll() {
+        this.operation = []; 
+    }
+    clearEntry() {
+        this.operation.pop(); 
+    }
+    setError() {
+        this.displayCalc = "Error"; 
+    }
+    
+    getLastOperation() {
+       return this.operation[this.operation.length -1];
+    }
+
+    setLastOperation(value) {
+        this.operation[this.operation.length -1] = value;
+    }
+
+    isOperation(value) {
+        return (['%', '*', '/', '-', '+'].indexOf(value) > -1) // => O typeof vai busca na array se existe os sinais, se ele achar vai trazer o index.
+    }
+
+    pushOperation(value) {
+        this.operation.push(value);
+
+        if(this.operation.length > 3) {
+            console.log(this.operation)
+        }
+    }
+
+    addOperation(value) {
+        if (isNaN(this.getLastOperation())) {  // String "true", aqui vai ser tratado os operadores.
+
+            if(this.isOperation(value)) {  
+                this.setLastOperation(value); // trocar o operador
+            } else {  
+                this.pushOperation(value);
+            }    
+        } else { //Number "false", aqui vai ser tratado os numeros.
+            if (this.isOperation(value)) {
+                this.pushOperation(value);
+            } else {
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue));
+            }
+        }
+        //console.log(this.operation)
+    }
+
+    execBtn(value) {
+        switch (value) {
+            case 'c':
+                this.clearAll();
+                break;
+
+            case 'e':
+                this.clearEntry();
+                break;
+
+            case 'porcento':
+                this.addOperation('%');
+                break;
+            case 'multi':
+                this.addOperation('*');
+                break;
+            case 'divisao':
+                this.addOperation('/');
+                break;
+
+            case 'subtr':
+                this.addOperation('-');
+                break;
+
+            case 'soma':
+                this.addOperation('+');
+                break;
+
+            case 'igual':
+                
+                break;
+
+            case 'ponto':
+                this.addOperation();
+                break;
+
+            case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '0':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+        }
+
+    }
     initButtonsEvents() {
         let buttons =  document.querySelectorAll('#buttons .col');
 
         buttons.forEach(button => {
             this.addEventListenerAll(button, 'click drag', () =>{
-                console.log(button.className.replace('col btn-',''));
+                let textBtn = button.className.replace('col btn-','');
+                this.execBtn(textBtn);
+
             });
         });
     }   
 
-    get displayCalc() { return this._displayCalcEl.innerHTML; }
-    set displayCalc(value) { this._displayCalcEl.innerHTML = value; }
+    // METODOS ACESSORES GETERS E SETERS
+    get displayCalc() {return this._displayCalcEl.innerHTML;}
+    set displayCalc(value) {this._displayCalcEl.innerHTML = value;}
 
-    get currentDate() { return new Date(); }
-    set currentDate(value) { this._currentDate = value; }
+    get currentDate() {return new Date();}
+    set currentDate(value) {this._currentDate = value;}
  
-    get dipslayDate() { return this._dateEl.innerHTML; }
-    set dipslayDate(value) { this._dateEl.innerHTML = value; }
+    get dipslayDate() {return this._dateEl.innerHTML;}
+    set dipslayDate(value) {this._dateEl.innerHTML = value;}
 
-    get displayTime() { return this._timeEl.innerHTML; }
-    set displayTime(value) { this._timeEl.innerHTML = value; }
+    get displayTime() {return this._timeEl.innerHTML;}
+    set displayTime(value) {this._timeEl.innerHTML = value;}
+
+    get operation() {return this._operation;}
+    set operation(value) {this._operation = value;}
 }
